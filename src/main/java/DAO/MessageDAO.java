d3b2bf9ba372b67c823a768290d9a40b31acc6ff
Emre Taskale <taskaleg@gmail.com>
2023-10-19 18:46:18 +0000

    create message tests passed

--- src/main/java/DAO/MessageDAO.java
+++ src/main/java/DAO/MessageDAO.java
@@ -13,7 +13,7 @@ public class MessageDAO {
         if(isValidMessage(message)){
             Connection connection = ConnectionUtil.getConnection();
             try{
-                String sql = "INSERT INTO account(posted_by, message_text, time_posted_epoch) VALUES(?, ?, ?)";
+                String sql = "INSERT INTO message(posted_by, message_text, time_posted_epoch) VALUES(?, ?, ?)";
                 PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 
                 preparedStatement.setInt(1, message.getPosted_by());
